3dc47ab0fa00d5188659a4160a03e996934e2e27
Suhani Awasthi <suhaniawasthi206@gmail.com>
2025-12-27 02:19:35 +0530

    pm2 scripts

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: 'voicebrief-api',
+            script: './backend/src/server.js',
+            cwd: '/home/ubuntu/VoiceBrief',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3000
+            },
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '500M'
+        }
+    ]
+};
